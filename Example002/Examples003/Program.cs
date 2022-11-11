bfc88c8a79015384dd7337114e84971ec38e42ec
Samat <snabsamatov@gmail.com>
2022-11-12 01:00:43 +0600

    Work

---
+++ Example002/Examples003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Console.WriteLine("Введите число; ");
+// int number = int.Parse(Console.ReadLine()!);
+// int sqr = number*number;
+// Console.WriteLine($"квадрат числа {number} = {sqr} ");
+
+// Console.WriteLine("Введите число ");
+// int N = int.Parse(Console.ReadLine()!);
+// int N2 = -N;
+// while (N2 <= N)
+// {
+//     Console.Write($"{N2} ");
+//     N2++;
+// }
+
+// Console.WriteLine("Введите числа ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите числа ");
+// int B = int.Parse(Console.ReadLine()!);
+// if (A==B/A)
+// {
+//     Console.Write("Да");
+// }else{Console.Write("Нет");
+   
+// }
+
+// Console.WriteLine("Введите числа ");
+// int P = int.Parse(Console.ReadLine()!);
+// if (P==1) Console.WriteLine("Понедельник ");
+// if (P==2) Console.WriteLine("Вторник ");
+// if (P==3) Console.WriteLine("Среда ");
+// if (P==4) Console.WriteLine("Четверг ");
+// if (P==5) Console.WriteLine("Пятница ");
+// if (P==6) Console.WriteLine("Субота ");
+// if (P==7) Console.WriteLine("Воскресенье ");
